0cb804cc0657b036bb921633f1f05fa80bdf6673
Yonas <yogiorno05@gmail.com>
2023-01-16 17:34:48 -0500

    Done Day 16

---
+++ Day-16/Part2.py
@@ -0,0 +1,42 @@
+with open("Inputs\Day16.txt", 'r') as q:
+    lines = q.read().strip().split("\n")
+    q.close()
+
+
+# The culprit's information
+ticker = {"children":3, "cats":7, "samoyeds":2, "pomeranians":3, "akitas":0, "vizslas":0, "goldfish":5, "trees":3, "cars":2, "perfumes":1}
+
+
+# Processing the information
+def readLines(input):
+    vals = input.replace(":", "").replace(",", "").split(" ")
+    return {"number":vals[1], vals[2]:int(vals[3]), vals[4]:int(vals[5]), vals[6]:int(vals[7])}
+
+
+
+# Finding the correct Sue
+def checkSue(sue):
+    global ticker
+    checks = list(sue.keys())
+    checks.pop(0)
+    for i in checks:
+        if i in ["cats", "trees"]:
+            if sue[i] <= ticker[i]: return False
+        elif i in ["pomeranians", "goldfish"]:
+            if sue[i] >= ticker[i]: return False
+        elif sue[i] != ticker[i]: return False
+    return True
+
+
+
+# Getting the information
+sues = []
+for line in lines:
+    sues.append(readLines(line))
+
+
+# Finding the right Sue, then printing out the result
+for sue in sues:
+    if checkSue(sue):
+        print("The gift is from Sue", sue["number"])
+        break
