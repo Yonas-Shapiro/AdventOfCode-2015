5f6ef5a44b553533e93b1b742d3891b59ffde3f3
Yonas <yogiorno05@gmail.com>
2023-01-16 20:18:51 -0500

    Done Day 20

---
+++ Day-20/Part2.py
@@ -0,0 +1,32 @@
+import math
+
+with open("Inputs\Day20.txt", 'r') as q:
+    lines = q.read().strip().split("\n")
+    line = int(lines[0])
+    q.close()
+
+
+# Finding all of the factors of a number
+def factorsSum(num):
+    factors = []
+    for i in range(1, int(math.sqrt(num))+1):
+        if num % i == 0: 
+            if int(num/i) <= 50:
+                factors.append(i)
+            if i <= 50:
+                factors.append(int(num/i))
+    num = sum(factors)
+    return num
+
+
+# Finding the lowest number that meets the requirements
+def calculateLowestNum(num):
+    house = 1
+    while True:
+        if factorsSum(house) >= num/11:
+            return house
+        house += 1
+        
+
+# Printing out the lowest number
+print("The lowest house number to recieve", line, "gifts is house number", calculateLowestNum(line))
