8496c3d655f134f5ec98f331072b6797ba050cb6
Yonas <yogiorno05@gmail.com>
2023-01-18 16:08:22 -0500

    Done Day 12 Part 2!

--- Days 10-19/Day-12/Part2.py
+++ Days 10-19/Day-12/Part2.py
@@ -1,80 +1,43 @@
+import json
+
 with open("Inputs\Day12.txt", 'r') as q:
     lines = q.read().strip().split("\n")
     q.close()
     line = lines[0]
 
 
-# Seeing if a character is an integer
-def checkIfInt(char):
+# Recursing through the dicts and lists
+def getVal(piece):
+    num = 0
     try:
-        x = int(char)
-        return True
+        if "red" in list(piece.values()): return 0
     except:
-        return False
-
-
-def splitAndCheck(input, depth):
-    for i in range(len(input)):
-        if input[i] == 
-
-
-
-
-
-
-# Getting all of the numbers from the input
-def getNums(input):
-    # Initializing the output array
-    output = []
-
-    # Going through the input
-    upto = -1
-    watch = 0
-    red = 0
-    temp = []
-    for i in range(len(input)):
-        # Making sure not to duplicate any numbers
-        if i <= upto:
-            continue
-        # Seeing if in an object
-        if input[i] == "{":
-            watch += 1
-        elif input[i] == "}":
-            print(watch, red)
-            print(temp)
-            if not red:
+        pass
+    for i in piece:
+        try:
+            x = int(i)
+            num += x
+        except:
+            try:
+                x = int(piece[i])
+                num += x
+            except:
+                try:
+                    if type(piece[i]) is dict:
+                        num += getVal(piece[i])
+                    elif type(piece[i]) is list:
+                        num += getVal(piece[i])
+                except:
+                    if type(i) is dict:
+                        num += getVal(i)
+                    elif type(i) is list:
+                        num += getVal(i)
+    return num
                 
-                for i in temp:
-                    output.append(temp.pop())
-            else:
-                for i in temp:
-                    temp.pop()
-            watch -= 1
-            red -= 1
-        
-        if watch and input[i:i+3] == "red" and i < len(input)-2:
-            red = True
-        # Ensuring all digits are accounted for
-        if checkIfInt(input[i]):
-            upto = i
-            while checkIfInt(input[upto]):
-                upto += 1
-            out = int(input[i:upto])
-
-            # Checking if the number is negative
-            if i > 0 and input[i-1] == "-":
-                out *= -1
-            temp.append(out)
-        if watch == False:
-            for i in temp:
-                output.append(temp.pop())
-    return output
 
 
+# Turning the input into a dict
+info = json.loads(line)
 
 
-
-
-
-
-print("The total sum is", sum(getNums(line)))
+print("The total sum of valid numbers is", getVal(info))
