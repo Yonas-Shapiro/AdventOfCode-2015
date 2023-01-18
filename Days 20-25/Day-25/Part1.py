239ba48893927564106fbac9530878ae7ae2b5dd
Yonas <yogiorno05@gmail.com>
2023-01-18 15:40:21 -0500

    Done Day 25 Part 1

--- Days 20-25/Day-25/Part1.py
+++ Days 20-25/Day-25/Part1.py
@@ -3,7 +3,33 @@ with open("Inputs\Day25.txt", 'r') as q:
     q.close()
     line = lines[0]
 
+
+# Getting the next location
+def getRow(prev):
+  if prev > 0:
+    return prev-1
+  else:
+    global grid
+    grid.append([])
+    return len(grid[0])
+
+def findLocationVal(location):
+  global grid
+  next = 0
+  val = 20151125
+  while True:
+    if len(grid) >= location[0] and len(grid[location[0]-1]) >= location[1]:
+      return grid[location[0]-1][location[1]-1]
+    next = getRow(next)
+    val = (val*252533)%33554393
+    grid[next].append(val)
+
+    
 # Getting the info
 vals = line.replace(".", "").replace(",", "").split(" ")
 location = [int(vals[-3]), int(vals[-1])]
-print(location)
+grid = [[20151125]]
+
+
+# Finding the value at the desired location
+print("The value at row", str(location[0]) + ", column", location[1], "is", findLocationVal(location))
